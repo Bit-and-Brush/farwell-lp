8e85c5c4b0f2971d2e878aedc10d5b7671d4982b
Rafael Willen <rafaelpadre20@gmail.com>
2025-07-10 19:46:38 +0100

    feat: setup node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
+import node from "@astrojs/node";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://farwell-lp.pages.dev",
   integrations: [sitemap(), react()],
+
   vite: {
     plugins: [tailwindcss()],
   },
   output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
