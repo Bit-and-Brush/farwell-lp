5f531536eeafef7d0f4d8c3693bdb234a5cef348
Rafael Willen <rafaelpadre20@gmail.com>
2025-07-10 19:37:34 +0100

    feat: remove cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
@@ -12,17 +11,6 @@ export default defineConfig({
   integrations: [sitemap(), react()],
   vite: {
     plugins: [tailwindcss()],
-    resolve: {
-      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
-      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
-      alias:
-        process.env.NODE_ENV === "production"
-          ? { "react-dom/server": "react-dom/server.edge" }
-          : undefined,
-    },
   },
   output: "server",
-  adapter: cloudflare({
-    imageService: "compile",
-  }),
 });
