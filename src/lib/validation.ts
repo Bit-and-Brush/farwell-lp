8baffd09fe3bf992a1c8db374e017a5d2c6197f0
Rafael Willen <rafaelpadre20@gmail.com>
2025-08-19 22:15:08 +0100

    feat: improve the validation of the firstName, lastName and add sanitization for the message field

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,15 +1,31 @@
+import sanitizeHTML from "sanitize-html";
 import { z } from "zod";
 
 export const contactFormValidationSchema = z.object({
-  firstName: z.string().min(1, "Nome muito curto"),
-  lastName: z.string().min(1, "Sobrenome muito curto"),
-  email: z.string().email("Email inválido"),
+  firstName: z
+    .string()
+    .trim()
+    .min(1, "Nome muito curto")
+    .regex(/^[\p{L} '-]+$/u, "Nome inválido"),
+  lastName: z
+    .string()
+    .trim()
+    .min(1, "Sobrenome muito curto")
+    .regex(/^[\p{L} '-]+$/u, "Sobrenome inválido"),
+  email: z.string().trim().toLowerCase().email("Email inválido"),
   phone: z
     .string()
-    .min(1, "Telefone muito curto")
-    .regex(/^\+244\d{9}$/, "Telefone inválido"),
+    .trim()
+    .regex(/^\d{9}$/, "Telefone inválido"),
   message: z
     .string()
+    .trim()
     .min(5, "Mensagem muito curta")
-    .max(500, "Mensagem muito longa"),
+    .max(500, "Mensagem muito longa")
+    .transform((message) =>
+      sanitizeHTML(message, {
+        allowedTags: [],
+        allowedAttributes: {},
+      }).trim(),
+    ),
 });
